37e31a0d5e1c0980cb807cfc591732903024dea3
Mahmoud Hassan <mahmoud_h_98@hotmail.com>
2025-10-25 13:53:13 +0300

    fixed Bg and some svg and added some css to make it clean

--- src/components/HomePage/HeroSection.jsx
+++ src/components/HomePage/HeroSection.jsx
@@ -1,46 +1,22 @@
 import React, { useState } from "react";
 import "./HeroSection.css";
-import { FaArrowAltCircleRight } from "react-icons/fa";
 
 const HeroSection = () => {
-  const [shelfVisible, setShelfVisible] = useState(false);
-
-  const toggleShelf = () => setShelfVisible(!shelfVisible);
-
   return (
     <div className="hero-section">
-      <img className="hero-section-table" src="/HeroSection/Table.svg" alt="" />
       <div className="hero-section-content">
-        <div className="hero-logo-wrapper">
-          <img className="hero-section-logo" src="/logo.png" alt="" />
-        </div>
+        <img className="hero-section-logo" src="/logo.png" alt="" />
         <img
           className="hero-section-Title"
           src="/HeroSection/Title.svg"
           alt=""
         />
-        <div className="hero-section-buttons">
-          <button>دوراتنا</button>
-          <button>خدماتنا</button>
-        </div>
+        <button className="hero-section-button">دوراتنا</button>
       </div>
-
-      {/* Arrow button */}
-      <button
-        className={`shelf-toggle ${shelfVisible ? "active" : ""}`}
-        onClick={toggleShelf}
-      >
-        <FaArrowAltCircleRight /> {/* Left arrow */}
-      </button>
-
-      {/* Shelf */}
-      <div className={`hero-shelfs-wrapper ${shelfVisible ? "show" : ""}`}>
-        <img
-          className="hero-section-shelfs"
-          src="/HeroSection/BookShelfs.svg"
-          alt=""
-        />
-        <img className="hero-compass" src="/HeroSection/Compass.svg" alt="" />
+      <div className="hero-section-shelf">
+        <div className="hero-section-top-shelf">
+          <img className="hero-shelf-compass" src="/HeroSection/Compass.svg" alt="" />
+        </div>
       </div>
     </div>
   );
